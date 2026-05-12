56c9534b169bdc19dc1a18b200ddc4d9d55d8cb4
divyaa0525 <divyanagasatya_poduri@srmap.edu.in>
2026-05-12 13:24:39 +0530

    updated return book functionality

--- server/src/main/java/com/library/management/controller/IssueController.java
+++ server/src/main/java/com/library/management/controller/IssueController.java
@@ -3,8 +3,12 @@ package com.library.management.controller;
 import com.library.management.dto.request.IssueBookRequest;
 import com.library.management.dto.response.ApiResponse;
 import com.library.management.dto.response.IssueResponse;
+import com.library.management.exception.BookAlreadyReturnedException;
+import com.library.management.exception.IssueRecordNotFoundException;
 import com.library.management.service.IssueService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,5 +27,28 @@ public class IssueController {
                 .message("Book issued successfully")
                 .data(issueService.issueBook(request))
                 .build();
+    }
+    @ExceptionHandler(IssueRecordNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiResponse<String> handleIssueRecordNotFound(
+        IssueRecordNotFoundException ex) {
+
+        return ApiResponse.<String>builder()
+            .success(false)
+            .message(ex.getMessage())
+            .data(null)
+            .build();
+    }
+
+    @ExceptionHandler(BookAlreadyReturnedException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponse<String> handleBookAlreadyReturned(
+        BookAlreadyReturnedException ex) {
+
+    return ApiResponse.<String>builder()
+            .success(false)
+            .message(ex.getMessage())
+            .data(null)
+            .build();
     }
 }
---
+++ server/src/main/java/com/library/management/exception/IssueRecordNotFoundException.java
@@ -0,0 +1,7 @@
+package com.library.management.exception;
+
+public class IssueRecordNotFoundException extends RuntimeException {
+    public IssueRecordNotFoundException(String message) {
+        super(message);
+    }
+}
